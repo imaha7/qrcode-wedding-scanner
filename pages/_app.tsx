78e2a36f5ddc3cf1c2f6b6bdc074f4ea56656257
imaha7 <ilham.maulana.07.0698@gmail.com>
2022-10-21 11:19:59 +0700

    Switch camera setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Montserrat",
+    }
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,70 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import React, { useState } from 'react';
 import { QrReader } from 'react-qr-reader';
-import { Box, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { Box, FormControlLabel, Switch, SwitchProps, ToggleButton, Typography, styled, useMediaQuery, useTheme } from '@mui/material';
+import { Camera } from '@mui/icons-material';
+
+const IOSSwitch = styled((props: SwitchProps) => (
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }: any) => ({
+  width: 42,
+  height: 26,
+  padding: 0,
+  '& .MuiSwitch-switchBase': {
+    padding: 0,
+    margin: 2,
+    transitionDuration: '300ms',
+    '&.Mui-checked': {
+      transform: 'translateX(16px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
+        opacity: 1,
+        border: 0,
+      },
+      '&.Mui-disabled + .MuiSwitch-track': {
+        opacity: 0.5,
+      },
+    },
+    '&.Mui-focusVisible .MuiSwitch-thumb': {
+      color: '#33cf4d',
+      border: '6px solid #fff',
+    },
+    '&.Mui-disabled .MuiSwitch-thumb': {
+      color:
+        theme.palette.mode === 'light'
+          ? theme.palette.grey[100]
+          : theme.palette.grey[600],
+    },
+    '&.Mui-disabled + .MuiSwitch-track': {
+      opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxSizing: 'border-box',
+    width: 22,
+    height: 22,
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 26 / 2,
+    backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+    opacity: 1,
+    transition: theme.transitions.create(['background-color'], {
+      duration: 500,
+    }),
+  },
+}));
 
 const Home: NextPage = () => {
   const [data, setData] = useState('No result');
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.up("sm"));
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChecked = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
 
   return (
     <Box sx={{ px: 2, py: 'auto' }}>
@@ -19,9 +74,10 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Box sx={{ width: !isMobile ? '100%' : '40%', border: '1px solid #2b2b2b', borderRadius: 5, mx: 'auto', my: 2 }}>
+
+      <Box sx={{ width: !isMobile ? '100%' : '40%', border: '1px solid #2b2b2b', borderRadius: 5, mx: 'auto', my: 2, px: 'auto' }}>
         <QrReader
-          constraints={{ facingMode: 'environment' }}
+          constraints={{ facingMode: checked ? 'user' : 'environment' }}
           scanDelay={100}
           onResult={(result, error) => {
             if (!!result) {
@@ -34,6 +90,13 @@ const Home: NextPage = () => {
           }}
           videoContainerStyle={{ width: '100%' }}
         />
+        <Box sx={{ width: '100%', mb: 2 }}>
+          <FormControlLabel
+            control={<IOSSwitch checked={checked} onChange={handleChecked} sx={{ m: 1 }} />}
+            label="Switch Camera"
+            sx={{ mx: 'auto' }}
+          />
+        </Box>
       </Box>
       <Typography align={'center'} variant={"body1"}>{data}</Typography>
     </Box>
